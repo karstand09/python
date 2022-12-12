64c50b2ba22a40476323fec7ef2612417a9d3a52
Stanislav Karpov <karstand09@gmail.com>
2022-12-12 21:28:32 +0300

    first commit

--- 1 lesson/1.py
+++ 1 lesson/1.py
@@ -1,1 +1,26 @@
-readme
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. 
+# Пример: - 6 -> да - 7 -> да - 1 -> нет
+
+
+def InputNumbers(inputText):
+    flag = False
+    while not flag:
+        try:
+            number = int(input(f"{inputText}"))
+            flag = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def checkNumber(num):
+    if 6 <= num <= 7:
+        print("Так то выходной")
+    elif 0 < num < 6:
+        print("Вали работать")
+    else:
+        print("В неделе 7 дней, Карл!")
+
+
+num = InputNumbers("Введите число: ")
+checkNumber(num)
